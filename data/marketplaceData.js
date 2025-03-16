8bf03ee74f98dfb575cf5ea9b9078c2a51b52ec7
Ahmad Hamza <priahmz@gmail.com>
2025-03-16 02:25:44 +0100

    Create marketplaceData.js

---
+++ data/marketplaceData.js
@@ -0,0 +1,25 @@
+const marketplaceData = [
+  {
+    id: 1,
+    name: "Unit 1",
+    price: "5,000,000 Naira",
+    roi: "1% daily",
+    piecesAvailable: 250,
+  },
+  {
+    id: 2,
+    name: "Unit 2",
+    price: "5,000,000 Naira",
+    roi: "1% daily",
+    piecesAvailable: 250,
+  },
+  {
+    id: 3,
+    name: "Unit 3",
+    price: "5,000,000 Naira",
+    roi: "1% daily",
+    piecesAvailable: 250,
+  },
+];
+
+export default marketplaceData;
